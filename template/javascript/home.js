2fe5380c6e4a99a5fb7cf639dbc15b218798eebf
gautam <gautamk173@gmail.com>
2017-03-30 15:35:39 +0530

    delete

--- template/javascript/home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $("#logout").click(function() {
-    // console.log("asddd");
-    sessionStorage.removeItem("email");
-    location.reload();
-    return;
-  });
-
-});
